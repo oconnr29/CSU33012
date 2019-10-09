fd45648f12160fb7f542bdb13d5d9cf11d3b5535
Ruairi <Ruairi@192.168.0.186>
2019-10-10 00:45:04 +0100

    Finished the findLCA class

--- Lowest Common Ancestor/src/LCA.java
+++ Lowest Common Ancestor/src/LCA.java
@@ -13,9 +13,19 @@ public class LCA {
 	Node findLCA(int n1, int n2) {
 		return findLCA(root, n1, n2);
 	}
+
 	Node findLCA(Node node, int n1, int n2) {
 		if (node == null)
 			return null;
-		return null;
+		else if (node.data == n1 || node.data == n2)
+			return node;
+		Node left = findLCA(node.left, n1, n2);
+		Node right = findLCA(node.right, n1, n2);
+		if (left != null && right != null)
+			return node;
+		else if (left != null)
+			return left;
+		else
+			return right;
 	}
 }
