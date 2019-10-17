7d4e58ea8535a20f9dc375015f093d62143058ab
Ruairi <Ruairi@192.168.0.186>
2019-10-17 21:54:49 +0100

    DagNode class created

--- Lowest Common Ancestor/src/DAG.java
+++ Lowest Common Ancestor/src/DAG.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
+class DagNode {
+	int data;
+	DagNode left;
+	DagNode right;
+	ArrayList<DagNode> ancestors;
+
+	DagNode(int value) {
+		data = value;
+		left = null;
+		right = null;
+		ancestors = null;
+	}
+}
 
 public class DAG {
-
+	
 }
